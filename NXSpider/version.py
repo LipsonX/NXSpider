68e588c0612d0ab2af3a820ff88ca24d698ceeb7
Lipson <LipsonChan@yahoo.com>
2018-12-17 23:36:03 +0800

    version to 0.0.5

--- NXSpider/version.py
+++ NXSpider/version.py
@@ -6,7 +6,7 @@
 #
 
 __title__ = 'NXSpider'
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 __description__ = 'NetEase X Spider, help u to have a better life!!!'
 __url__ = 'https://github.com/Grass-CLP/NXSpider'
 __author__ = 'LipsonChan'
